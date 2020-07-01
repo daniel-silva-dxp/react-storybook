5b58db4475a38a8f81b69f6825e76a558a5b7deb
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-01 11:26:57 -0300

    feat: iniciando as configurações do Storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'dark',
+
+    colorSecondary: "#7159c1",
+    
+    appBg: "#0b0a0d",
+    appContentBg: "#16161a",
+    appBorderColor: "#7159c1",
+    
+    textColor: "#fff",
+
+    // Toolbar default and active colors
+    barTextColor: "#e6e6e6",
+    barSelectedColor: "#fff",
+    barBg: "#1b1a1f",
+
+    brandTitle: "UI Docs",
+    brandUrl: "https://recketseat.com.br"
+})
