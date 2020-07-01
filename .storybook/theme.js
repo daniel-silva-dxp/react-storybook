d47a38aab136e003650c7d1d1b08a780b7ea3370
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-01 15:41:59 -0300

    feat: alterando o tema do storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,21 +1,36 @@
 import { create } from '@storybook/theming';
 
 export default create({
-    base: 'dark',
+    base: 'light',
 
     colorSecondary: "#28F5BE",
     
+    // UI
     appBg: "#fff",
     appContentBg: "#f8f8f8",
     appBorderColor: "#28F5BE",
+    appBorderRadius: 3,
     
+    // Typography
+    fontBase: '"Roboto Mono", sans-serif',
+    fontCode: 'monospace',
+
+    // Text colors
     textColor: "#1b1a1f",
+    textInverseColor: 'rgba(255,255,255,0.9)',
 
     // Toolbar default and active colors
     barTextColor: "#e6e6e6",
     barSelectedColor: "#fff",
     barBg: "#1b1a1f",
 
+    // Form colors
+    inputBg: '#f8f8f8',
+    inputBorder: '#28f5be',
+    inputTextColor: '#1b1a1f',
+    inputBorderRadius: 3,
+
     brandTitle: "Daniel Silva",
-    brandUrl: "https://danielsilva-front.netlify.app/"
+    brandUrl: "https://danielsilva-front.netlify.app/",
+    brandImage: 'https://raw.githubusercontent.com/daniel-silva-dxp/danielsilva-dev/master/src/img/png/logo.png',
 })
