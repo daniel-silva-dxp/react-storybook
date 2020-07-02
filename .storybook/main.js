3c2b46e74ff6a7f76a1c60035d2ff91ad5c001ec
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-02 18:28:20 -0300

    feat: configurando o storybook para typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,21 @@
+const path = require('path');
+
 module.exports = {
-    stories: ['../src/**/*.stories.tsx', '../src/**/*.stories.[tj]s'],
-    webpackFinal: async config => {
-      config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: require.resolve('ts-loader'),
-          },
-          {
-            loader: require.resolve('react-docgen-typescript-loader'),
-          },
-        ],
-      });
-      config.resolve.extensions.push('.ts', '.tsx');
-      return config;
-    },
+	stories: [ '../src/**/*.stories.tsx', '../src/**/*.stories.[tj]s' ],
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			include: [ path.resolve(__dirname, '..') ],
+			use: [
+				{
+					loader: require.resolve('ts-loader')
+				},
+				{
+					loader: require.resolve('react-docgen-typescript-loader')
+				}
+			]
+		});
+		config.resolve.extensions.push('.ts', '.tsx');
+		return config;
+	}
 };
