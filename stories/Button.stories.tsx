0ae0e0e7980c3231435e0923a00bd3d18765d12a
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-02 18:30:46 -0300

    feat: alterando background e color do button

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,4 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '../src';
 
-storiesOf('Button', module).add('Default', () => <Button/>)
+storiesOf('Button', module).add('Default', () => (
+	<Button backgroundColor="#28f5be" color="#1e1e1e">
+		TESTE
+	</Button>
+));
