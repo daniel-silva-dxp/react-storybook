efd495ae5a2d7927832cd1fbf51243ca0f0e4c9d
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-02 18:31:47 -0300

    feat: criando o component separator

--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MyButton = ({ children }) => {
-    return (
-        <button>{children}</button>
-    );
-};
-
-export default MyButton;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+interface Props {
+	/** 
+	 * This prop is brackground-color from default button
+	*/
+	backgroundColor?: string;
+	/** 
+	 * This prop is text color from default button
+	*/
+	color?: string;
+}
+
 const style: React.CSSProperties = {
-	backgroundColor: '#7159c1',
-	color: '#fff'
+	backgroundColor: '#28f5be',
+	color: '#1e1e1e'
 };
 
-export const Button: React.FC = ({ children }) => {
+export const Button: React.FC<Props> = ({ children, backgroundColor = '#28f5be', color = '#1e1e1e' }) => {
 	return <button style={style}>{children}</button>;
 };
