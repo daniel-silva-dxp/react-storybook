5785a03bb8386a12efd496e0d1965dcbf3d1ee5d
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-02 18:28:50 -0300

    feat: alterando o theme do storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,36 +1,36 @@
 import { create } from '@storybook/theming';
 
 export default create({
-    base: 'light',
+	base: 'light',
 
-    colorSecondary: "#28F5BE",
-    
-    // UI
-    appBg: "#fff",
-    appContentBg: "#f8f8f8",
-    appBorderColor: "#28F5BE",
-    appBorderRadius: 3,
-    
-    // Typography
-    fontBase: '"Roboto Mono", sans-serif',
-    fontCode: 'monospace',
+	colorSecondary: '#28F5BE',
 
-    // Text colors
-    textColor: "#1b1a1f",
-    textInverseColor: 'rgba(255,255,255,0.9)',
+	// UI
+	appBg: '#fff',
+	appContentBg: '#f8f8f8',
+	appBorderColor: '#28F5BE',
+	appBorderRadius: 3,
 
-    // Toolbar default and active colors
-    barTextColor: "#e6e6e6",
-    barSelectedColor: "#fff",
-    barBg: "#1b1a1f",
+	// Typography
+	fontBase: '"Roboto Mono", sans-serif',
+	fontCode: 'monospace',
 
-    // Form colors
-    inputBg: '#f8f8f8',
-    inputBorder: '#28f5be',
-    inputTextColor: '#1b1a1f',
-    inputBorderRadius: 3,
+	// Text colors
+	textColor: '#1e1e1e',
+	textInverseColor: 'rgba(255,255,255,0.9)',
 
-    brandTitle: "Daniel Silva",
-    brandUrl: "https://danielsilva-front.netlify.app/",
-    brandImage: 'https://raw.githubusercontent.com/daniel-silva-dxp/danielsilva-dev/master/src/img/png/logo.png',
-})
+	// Toolbar default and active colors
+	barTextColor: '#e6e6e6',
+	barSelectedColor: '#fff',
+	barBg: '#1e1e1e',
+
+	// Form colors
+	inputBg: '#232323',
+	inputBorder: '#28f5be',
+	inputTextColor: '#1e1e1e',
+	inputBorderRadius: 3,
+
+	brandTitle: 'Daniel Silva',
+	brandUrl: 'https://danielsilva-front.netlify.app/',
+	brandImage: 'https://raw.githubusercontent.com/daniel-silva-dxp/danielsilva-dev/master/src/img/png/logo.png'
+});
